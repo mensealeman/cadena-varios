4807b70bef02bd2df05b34ec7c5182631de3860d
Ruben Melian Aleman <ruben.melian.aleman@gmail.com>
2023-12-14 20:39:29 +0000

    12

--- String 12/src/String12.java
+++ String 12/src/String12.java
@@ -1,2 +1,56 @@
+import java.util.Scanner;
 public class String12 {
+    public static void main(String[] args) {
+
+/*
+        12. Definir una string a con la cadena «Esto es una string»; definir la string b con la cadena «Y
+        esto también».
+        1 • Comparar las dos strings y decir si son iguales
+        2 • Crear la string «c» con la concatenación de a y b
+        3 • Crear la string «d» con la concatenacion de a y b otra vez
+        4 • Comparar las string c y d y decir si son iguales
+        5 • Indicar cuantos caracteres tiene cada una de las strings
+        6 • Copiar la string a en la string b
+        7 • Presentar en consola el contenido de la string a y de la string b.
+ */
+
+        String a = "Esto es una string.";
+        String b = "Y esto también.";
+
+    // 1. Comparar las dos strings y decir si son iguales.
+
+        if (a.equals(b)) {
+            System.out.println("Son iguales.");
+        } else {
+            System.out.println("No son iguales.");
+        }
+
+    // 2. Crear la string «c» con la concatenación de a y b.
+
+        String c = a + b;
+
+    // 3. Crear la string «d» con la concatenacion de a y b otra vez.
+
+        String d = a + b;
+
+    // 4. Comparar las string c y d y decir si son iguales.
+
+        if (c.equals(d)) {
+            System.out.println("Son iguales");
+        } else {
+            System.out.println("No son iguales.");
+        }
+
+    // 5. Indicar cuantos caracteres tiene cada una de las strings.
+
+        System.out.println("La string a tiene "+a.length()+" caracteres. Y la string b tiene "+b.length()+" caracteres.");
+
+    // 6. Copiar la string a en la string b
+
+
+
+
+    // 7. Presentar en consola el contenido de la string a y de la string b.
+
+    }
 }
